d33e9359c8de71cc183b5056471efb5e89f9c1a2
5ahithra <sahithra@outlook.com>
2023-10-17 12:01:24 -0500

    10/17/2023 Work in Progress

---
+++ app/src/main/java/com/example/calendarapplication/CalendarAdapter.java
@@ -0,0 +1,63 @@
+package com.example.calendarapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder> {
+    private final ArrayList<String> daysOfMonth;
+    private final OnItemListener onItemListener;
+    private final LocalDate selectedDate;
+    private final ArrayList<LocalDate> periodDays;
+
+    public CalendarAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener, LocalDate selectedDate, ArrayList<LocalDate> periodDays) {
+        this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
+        this.selectedDate = selectedDate;
+        this.periodDays = periodDays;
+    }
+
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.calendar_cell, parent, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        return new CalendarViewHolder(view, onItemListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+        String dayText = daysOfMonth.get(position);
+        holder.dayOfMonth.setText(dayText);
+
+        if (!dayText.isEmpty()) {
+            int day = Integer.parseInt(dayText);
+            LocalDate currentDate = LocalDate.of(selectedDate.getYear(), selectedDate.getMonthValue(), day);
+
+            if (periodDays.contains(currentDate)) {
+                holder.dayOfMonth.setBackgroundResource(R.drawable.red_circle);
+            } else {
+                holder.dayOfMonth.setBackgroundResource(R.drawable.empty_cell);
+            }
+        } else {
+            holder.dayOfMonth.setBackgroundResource(0);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return daysOfMonth.size();
+    }
+
+    public interface OnItemListener {
+        void onItemClick(int position, String dayText);
+    }
+}
---
+++ app/src/main/java/com/example/calendarapplication/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.calendarapplication;
+
+import com.example.calendarapplication.CalendarAdapter;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
+    private ArrayList<LocalDate> periodDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
+        periodDays = new ArrayList<>();
+    }
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this, selectedDate, periodDays);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+        int daysInMonth = yearMonth.lengthOfMonth();
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for (int i = 1; i <= 42; i++) {
+            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
+                daysInMonthArray.add("");
+            } else {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return daysInMonthArray;
+    }
+
+    private String monthYearFromDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public void previousMonthAction(View view) {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    public void nextMonthAction(View view) {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if (!dayText.isEmpty()) {
+            int day = Integer.parseInt(dayText);
+            selectedDate = LocalDate.of(selectedDate.getYear(), selectedDate.getMonthValue(), day);
+            showConfirmationDialog(selectedDate);
+        }
+    }
+
+    private void showConfirmationDialog(final LocalDate selectedDate) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm Period Start Date")
+                .setMessage("Do you want to log this date as the start of your period?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Handle confirming the selected date as the start of the period
+                        // For example, you can store the date and perform necessary actions.
+                        String message = "Period Start Date: " + selectedDate.toString();
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User declined, do nothing
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+}
+
